177e8a1c6a0432ae5a2014e84c10718ad73712ff
Fajar andrianto <106133650+FajarAndrianto037@users.noreply.github.com>
2023-06-15 18:26:06 +0700

    Update uas_psd.py

--- uas_psd.py
+++ uas_psd.py
@@ -56,6 +56,23 @@ with Data:
 
 
 with Preproses:
+   # transform univariate time series to supervised learning problem
+   from numpy import array
+    # split a univariate sequence into samples
+   def split_sequence(sequence, n_steps):
+      X, y = list(), list()
+      for i in range(len(sequence)):
+      # find the end of this pattern
+         end_ix = i + n_steps
+      # check if we are beyond the sequence
+         if end_ix > len(sequence)-1:
+               break
+      # gather input and output parts of the pattern
+         # print(i, end_ix)
+         seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+         X.append(seq_x)
+         y.append(seq_y)
+      return array(X), array(y)
    #tuning data
    n_steps = 5
    X, y = split_sequence(df_close, n_steps)
